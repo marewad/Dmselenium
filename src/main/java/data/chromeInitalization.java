7f149c3211cd3b6f9dc39195b59a8730671dc19b
Mauli <dnyaneshwarmarewad008@gmail.com>
2024-09-25 16:59:31 +0530

    added selenium grid

--- src/main/java/data/chromeInitalization.java
+++ src/main/java/data/chromeInitalization.java
@@ -1,7 +1,13 @@
 package data;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import com.epam.healenium.SelfHealingDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -9,12 +15,15 @@ public class chromeInitalization {
 
 	//String screenValue = "cargo";
 	WebDriver driver;
-	public WebDriver initalization() throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
-		WebDriver delegate = new ChromeDriver();
-		SelfHealingDriver driver = SelfHealingDriver.create(delegate);
-		driver.manage().window().maximize();
-		driver.get("https://vaadin.dev.dawinci.cloud");
-		return driver;
+	public WebDriver initalization() throws InterruptedException, MalformedURLException {
+	   WebDriverManager.chromedriver().setup();
+       DesiredCapabilities caps = new DesiredCapabilities();
+       caps.setBrowserName("chrome");
+        WebDriver remoteDriver = new RemoteWebDriver(new URL("http://192.168.1.11:4444"), caps);
+        SelfHealingDriver selfHealingDriver = SelfHealingDriver.create(remoteDriver);
+        selfHealingDriver.manage().window().maximize();
+        selfHealingDriver.get("https://vaadin.dev.dawinci.cloud");
+
+        return selfHealingDriver; //
 	}
 }
