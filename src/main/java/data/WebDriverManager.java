5e982c5fc3661893ecef8e546ef1d507327a4eaa
Mauli <dnyaneshwarmarewad008@gmail.com>
2024-10-09 19:10:07 +0530

    Updated pom.xml and added testng.xml configuration

--- src/main/java/data/WebDriverManager.java
+++ src/main/java/data/WebDriverManager.java
@@ -30,7 +30,7 @@ public class WebDriverManager {
 	   System.setProperty("webdriver.chrome.driver", "C:/Users/dnyaneshwar.marewad/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
        DesiredCapabilities caps = new DesiredCapabilities();
        caps.setBrowserName("chrome");
-        WebDriver remoteDriver = new RemoteWebDriver(new URL(" http://192.168.1.3:4444"), caps);
+        WebDriver remoteDriver = new RemoteWebDriver(new URL(" http://192.168.1.5:4444"), caps);
         SelfHealingDriver selfHealingDriver = SelfHealingDriver.create(remoteDriver);
         selfHealingDriver.manage().window().maximize();
         selfHealingDriver.get("https://vaadin.dev.dawinci.cloud");
